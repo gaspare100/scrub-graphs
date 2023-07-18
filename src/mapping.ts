e59d0e1a99f3c122997a839b02ae00e1b5f11bcb
theg <theg>
2023-07-18 16:47:08 +0100

    fix

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,10 +1,12 @@
-import { Deposit } from "../generated/WindAndCheck/WindAndCheck";
+import { Deposit, DepositCall } from "../generated/WindAndCheck/WindAndCheck";
 import { VaultDeposit } from "../generated/schema";
 import { log } from "@graphprotocol/graph-ts";
-export function handleNewDeposit(event: Deposit): void {
+
+export function handleCreateGravatar(call: DepositCall): void {
   log.info("New deposit detected!", []);
-  let vaultDeposit = new VaultDeposit(event.transaction.hash);
-  vaultDeposit.user = event.params.user;
-  vaultDeposit.amount = event.params.amount;
+  let vaultDeposit = new VaultDeposit(call.transaction.hash);
+
+  vaultDeposit.user = call.from;
+  vaultDeposit.amount = call.inputs.amount;
   vaultDeposit.save();
 }
