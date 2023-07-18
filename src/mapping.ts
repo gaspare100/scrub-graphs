16e8ed214ce5bc5693899744ef8f754bfb8c83e3
theg <theg>
2023-07-18 16:39:37 +0100

    fix

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,7 +3,6 @@ import { VaultDeposit } from "../generated/schema";
 import { log } from "@graphprotocol/graph-ts";
 export function handleNewDeposit(event: Deposit): void {
   log.info("New deposit detected!", []);
-  throw new Error("Not implemented");
   let vaultDeposit = new VaultDeposit(event.transaction.hash.toHex());
   vaultDeposit.user = event.params.user;
   vaultDeposit.amount = event.params.amount;
