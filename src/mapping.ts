bbb701b377e00c4f1cfe4acec3f7b6f0bb708ff4
theg <theg>
2023-07-18 16:21:13 +0100

    log

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,9 +2,9 @@ import { Deposit } from "../generated/WindAndCheck/WindAndCheck";
 import { VaultDeposit } from "../generated/schema";
 import { log } from "@graphprotocol/graph-ts";
 export function handleNewDeposit(event: Deposit): void {
+  log.info("New deposit detected!", []);
   let vaultDeposit = new VaultDeposit(event.transaction.hash.toHex());
   vaultDeposit.user = event.params.user;
   vaultDeposit.amount = event.params.amount;
-  log.info("New deposit: {}", [vaultDeposit.id]);
   vaultDeposit.save();
 }
