c242e090760527648c814f918d7d98fca60b4779
theg <theg>
2023-07-19 18:49:01 +0100

    new graphstructure

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,11 +1,58 @@
-import { Deposit } from "../generated/WindAndCheck/WindAndCheck";
-import {  VaultDeposit } from "../generated/schema";
+import {
+  Deposit,
+  RewardDistribution,
+  Withdraw,
+} from "../generated/WindAndCheck/WindAndCheck";
+import {
+  Vault,
+  VaultDeposit,
+  VaultReward,
+  VaultWithdraw,
+} from "../generated/schema";
 import { log } from "@graphprotocol/graph-ts";
 
 export function handleNewDeposit(event: Deposit): void {
   log.info("New deposit detected!", []);
-  let vaultDeposit = new VaultDeposit(event.transaction.hash.toHex()+"-"+event.logIndex.toString());
+  let vaultDeposit = new VaultDeposit(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
   vaultDeposit.user = event.params.user;
   vaultDeposit.amount = event.params.amount;
+  vaultDeposit.vault = event.address.toHex();
+  const vault = Vault.load(event.address.toHex());
+
+  vault?.tvl.plus(event.params.amount);
+  vault?.save();
   vaultDeposit.save();
+}
+
+export function handleNewWithdraw(event: Withdraw): void {
+  log.info("New withdraw detected!", []);
+  let vaultWithdraw = new VaultWithdraw(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  vaultWithdraw.user = event.params.user;
+  vaultWithdraw.amount = event.params.amount;
+  vaultWithdraw.vault = event.address.toHex();
+  vaultWithdraw.save();
+  const vault = Vault.load(event.address.toHex());
+  vault?.tvl.minus(event.params.amount);
+  vault?.save();
+}
+
+export function handleNewReward(event: RewardDistribution): void {
+  log.info("New reward detected!", []);
+  let vaultReward = new VaultReward(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  vaultReward.reward = event.params.amount;
+  vaultReward.apr = event.params.apy;
+  vaultReward.vault = event.address.toHex();
+  vaultReward.save();
+  const vault = Vault.load(event.address.toHex());
+  if (vault) {
+    vault.tvl.plus(event.params.amount);
+    vault.apr = event.params.apy;
+    vault.save();
+  }
 }
