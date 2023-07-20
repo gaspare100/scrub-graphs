cfaa9e9309a161a6f232992a70bfdb8539eadda1
theg <theg>
2023-07-20 13:56:19 +0100

    new graphstructure

--- src/mapping.ts
+++ src/mapping.ts
@@ -13,10 +13,10 @@ import {
   VaultReward,
   VaultWithdraw,
 } from "../generated/schema";
-import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes, DataSourceContext, log } from "@graphprotocol/graph-ts";
+import { dataSource } from "@graphprotocol/graph-ts";
 
 export function handleNewVault(event: NewVault): void {
-  WindAndCheck.create(event.params.vault);
   log.info("New vault detected!", []);
   let vault = Vault.load(event.params.vault.toHex());
   if (!vault) {
@@ -28,46 +28,68 @@ export function handleNewVault(event: NewVault): void {
   vault.apr = BigInt.fromI32(0);
   vault.tokenName = event.params.tokenName;
   vault.save();
+  let context = new DataSourceContext();
+  context.setBigInt("decimals", vault.decimals);
+  context.setBytes("underlying", vault.underlying);
+  context.setString("tokenName", vault.tokenName);
+  WindAndCheck.createWithContext(event.params.vault, context);
 }
 
 export function handleNewDeposit(event: Deposit): void {
   log.info("New deposit detected!", []);
+  let context = dataSource.context();
+
   let vaultDeposit = new VaultDeposit(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
   vaultDeposit.user = event.params.user;
-  vaultDeposit.amount = event.params.amount;
+  vaultDeposit.amount = event.params.amount.div(
+    BigInt.fromI32(context.getBigInt("decimals"))
+  );;
   vaultDeposit.vault = event.address.toHex();
   vaultDeposit.save();
   const vault = Vault.load(event.address.toHex());
   if (vault) {
-    vault.tvl.plus(event.params.amount);
+    vault.tvl.plus(event.params.amount.div(
+      BigInt.fromI32(context.getBigInt("decimals"))
+    ););
     vault.save();
   }
 }
 
 export function handleNewWithdraw(event: Withdraw): void {
   log.info("New withdraw detected!", []);
+  let context = dataSource.context();
+
   let vaultWithdraw = new VaultWithdraw(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
   vaultWithdraw.user = event.params.user;
-  vaultWithdraw.amount = event.params.amount;
+  vaultWithdraw.amount = event.params.amount.div(
+    BigInt.fromI32(context.getBigInt("decimals"))
+  );;
   vaultWithdraw.vault = event.address.toHex();
   vaultWithdraw.save();
   const vault = Vault.load(event.address.toHex());
   if (vault) {
-    vault.tvl.minus(event.params.amount);
+    vault.tvl.minus(event.params.amount.div(
+      BigInt.fromI32(context.getBigInt("decimals"))
+    ););
     vault.save();
   }
 }
 
 export function handleNewReward(event: RewardDistribution): void {
   log.info("New reward detected!", []);
+  let context = dataSource.context();
+
   let vaultReward = new VaultReward(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
-  vaultReward.reward = event.params.amount;
+
+  vaultReward.reward = event.params.amount.div(
+    BigInt.fromI32(context.getBigInt("decimals"))
+  );
   vaultReward.apr = event.params.apy.div(BigInt.fromI32(10000));
   vaultReward.vault = event.address.toHex();
   vaultReward.save();
