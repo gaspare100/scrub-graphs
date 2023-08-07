419d62ce5d7a8fc50866a2e6a6547489009b72a2
theg <theg>
2023-08-07 18:46:05 +0100

    track votes

--- src/mapping.ts
+++ src/mapping.ts
@@ -113,7 +113,7 @@ export function handleNewReward(event: RewardDistribution): void {
   const vault = Vault.load(event.address.toHex());
 }
 
-export function handleNewLock(call: Create_lockCall) {
+export function handleNewLock(call: Create_lockCall): void {
   log.info("New lock detected!", []);
   let newLock = new Lock(call.outputs.value0.toString());
   newLock.nftID = call.outputs.value0;
@@ -123,7 +123,38 @@ export function handleNewLock(call: Create_lockCall) {
   newLock.save();
 }
 
-export function handleNewVote(call: VoteCall) {
+export function handleNewVote(call: VoteCall): void {
   log.info("New vote detected!", []);
- 
+  let lock = Lock.load(
+    call.inputs.tokenId + "-" + call.transaction.hash.toHex()
+  );
+
+  if (lock != null) {
+    let totalWeight = BigInt.fromI32(0);
+    for (let i = 0; i < call.inputs._weights.length; i++) {
+      totalWeight = totalWeight.plus(call.inputs._weights[i]);
+    }
+    for (let i = 0; i < call.inputs._poolVote.length; i++) {
+      let newVote = new Vote(
+        call.inputs.tokenId +
+          "-" +
+          call.inputs._poolVote[i] +
+          "-" +
+          call.transaction.hash.toHex() +
+          "-" +
+          i
+      );
+
+      newVote.nftID = call.inputs.tokenId;
+      newVote.user = call.from;
+      newVote.pool = call.inputs._poolVote[i];
+      newVote.amount = call.inputs._weights[i]
+        .times(lock?.amount)
+        .div(totalWeight);
+      newVote.timestamp = call.block.timestamp;
+      newVote.save();
+    }
+  } else {
+    log.info("Lock not found! Not tracking", []);
+  }
 }
