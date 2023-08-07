958f57e8cc71e7878715f993af13fe0dd355e6cd
theg <theg>
2023-08-07 22:06:32 +0100

    track votes

--- src/mappingVotes.ts
+++ src/mappingVotes.ts
@@ -19,9 +19,7 @@ export function handleNewLock(event: Deposit): void {
 export function handleNewVote(event: Voted): void {
   log.info("New vote detected!", []);
 
-  let lock = Lock.load(
-    event.params.tokenId.toString() + "-" + event.transaction.hash.toHex()
-  );
+  let lock = Lock.load(event.params.tokenId.toString());
 
   if (lock != null) {
     let newVote = new Vote(
@@ -31,7 +29,14 @@ export function handleNewVote(event: Voted): void {
     newVote.nftID = event.params.tokenId;
     newVote.user = event.transaction.from;
     let voter = Voter.bind(event.address);
-
+    let votes = voter.votes(
+      event.params.tokenId,
+      Address.fromHexString("0x78Ef6D3E3d0da9B2248C11BE11743B4C573ADd25")
+    );
+    newVote.pool = Address.fromHexString(
+      "0x78Ef6D3E3d0da9B2248C11BE11743B4C573ADd25"
+    );
+    newVote.amount = votes;
     newVote.timestamp = event.block.timestamp;
     newVote.save();
   } else {
