4acc3308d0a80d1689eb9ee99f1ea9d6b44a89bc
theg <theg>
2023-08-07 22:01:51 +0100

    track votes

--- src/mappingVotes.ts
+++ src/mappingVotes.ts
@@ -31,14 +31,7 @@ export function handleNewVote(event: Voted): void {
     newVote.nftID = event.params.tokenId;
     newVote.user = event.transaction.from;
     let voter = Voter.bind(event.address);
-    let votes = voter.votes(
-      event.params.tokenId,
-      Address.fromHexString("0x78Ef6D3E3d0da9B2248C11BE11743B4C573ADd25")
-    );
-    newVote.pool = Address.fromHexString(
-      "0x78Ef6D3E3d0da9B2248C11BE11743B4C573ADd25"
-    );
-    newVote.amount = votes;
+
     newVote.timestamp = event.block.timestamp;
     newVote.save();
   } else {
