650462c52206fe757cf10e8fb829568ffdbc8198
theg <theg>
2023-08-08 10:15:04 +0100

    track votes

--- src/mappingVotes.ts
+++ src/mappingVotes.ts
@@ -6,7 +6,10 @@ import { Address, BigInt, Bytes, log } from "@graphprotocol/graph-ts";
 
 export function handleNewLock(event: Deposit): void {
   log.info("New lock detected!", []);
-  if (Lock.load(event.params.tokenId.toString()) == null) {
+  if (
+    Lock.load(event.params.tokenId.toString()) == null &&
+    event.params.deposit_type == BigInt.fromI32(1)
+  ) {
     let newLock = new Lock(event.params.tokenId.toString());
     newLock.nftID = event.params.tokenId;
     newLock.user = event.transaction.from;
@@ -19,30 +22,22 @@ export function handleNewLock(event: Deposit): void {
 export function handleNewVote(event: Voted): void {
   log.info("New vote detected!", []);
 
-  let lock = Lock.load(event.params.tokenId.toString());
-
-  if (lock != null) {
-    log.info("Lock Found !", []);
-    let newVote = new Vote(
-      event.params.tokenId.toString() + "-" + event.transaction.hash.toHex()
-    );
-
-    newVote.nftID = event.params.tokenId;
-    newVote.user = event.transaction.from;
-    let voter = Voter.bind(event.address);
-    let votes = voter.votes(
-      event.params.tokenId,
-      Address.fromBytes(
-        Bytes.fromHexString("0x78Ef6D3E3d0da9B2248C11BE11743B4C573ADd25")
-      )
-    );
-    newVote.pool = Address.fromBytes(
+  let newVote = new Vote(
+    event.params.tokenId.toString() + "-" + event.transaction.hash.toHex()
+  );
+  newVote.nftID = event.params.tokenId;
+  newVote.user = event.transaction.from;
+  let voter = Voter.bind(event.address);
+  let votes = voter.votes(
+    event.params.tokenId,
+    Address.fromBytes(
       Bytes.fromHexString("0x78Ef6D3E3d0da9B2248C11BE11743B4C573ADd25")
-    );
-    newVote.amount = votes;
-    newVote.timestamp = event.block.timestamp;
-    newVote.save();
-  } else {
-    log.info("Lock not found! Not tracking", []);
-  }
+    )
+  );
+  newVote.pool = Address.fromBytes(
+    Bytes.fromHexString("0x78Ef6D3E3d0da9B2248C11BE11743B4C573ADd25")
+  );
+  newVote.amount = votes;
+  newVote.timestamp = event.block.timestamp;
+  newVote.save();
 }
