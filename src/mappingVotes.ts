8af8828d6d7904b0af3d9a2f86a1b01ea65d2eab
theg <theg>
2023-08-08 10:22:28 +0100

    track votes

--- src/mappingVotes.ts
+++ src/mappingVotes.ts
@@ -8,7 +8,7 @@ export function handleNewLock(event: Deposit): void {
   log.info("New lock detected!", []);
   if (
     Lock.load(event.params.tokenId.toString()) == null &&
-    BigInt.fromI32(1).equals(event.params.deposit_type)
+    BigInt.fromI32(1).equals(BigInt.fromI32(event.params.deposit_type))
   ) {
     let newLock = new Lock(event.params.tokenId.toString());
     newLock.nftID = event.params.tokenId;
